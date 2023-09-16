07a0eb4d8462540fb2e5027dfa7153cf0d600353
Sergei Biriukov <svbiriukov@gmail.com>
2023-09-16 19:31:54 +1000

    finish with poc

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"log/slog"
 	"net/http"
 	"os"
+	"os/exec"
 	"strings"
 
 	"gopkg.in/yaml.v3"
@@ -46,9 +47,10 @@ func (s Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 	defer r.Body.Close()
 
+	slog.Debug("incomming request body: " + fmt.Sprintf("%+v", string(bytes)))
 	var notification Notification
 	if err := json.Unmarshal(bytes, &notification); err != nil {
-		slog.Error("cannot unmarshal incomming notification: " + fmt.Sprint(notification))
+		slog.Error("cannot unmarshal incomming notification: " + fmt.Sprintf("%+v", string(bytes)))
 		slog.Error(err.Error())
 	}
 
@@ -56,22 +58,51 @@ func (s Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if len(notification.Alerts) > 0 {
 		for _, alert := range notification.Alerts {
 			if alertName, ok := alert.Labels[alertNameLabel]; ok {
-				slog.Debug("processing alert [" + alertName + "]")
+				slog.Debug("processing alert: " + alertName)
 				for _, action := range s.Config.Actions {
 					if action.Alertname == alertName {
-						slog.Debug("processing action: [" + fmt.Sprint(action) + "]")
+						slog.Debug("command template: " + fmt.Sprint(action.Command))
+
+						labelValues := make(map[string]string)
+						for _, cmdWord := range strings.Split(action.Command, " ") {
+							if strings.HasPrefix(cmdWord, "~") {
+								if value, ok := alert.Labels[strings.TrimPrefix(cmdWord, "~")]; ok {
+									labelValues[cmdWord] = value
+								} else {
+									slog.Error("no label '" + strings.TrimPrefix(cmdWord, "~") + "' in alert, skipping alert: " + fmt.Sprintf("%+v", alert) + " and action: " + fmt.Sprintf("%+v", action))
+									return
+								}
+							}
+						}
+
+						for k, v := range labelValues {
+							action.Command = strings.ReplaceAll(action.Command, k, v)
+						}
+
+						slog.Debug("processing command: " + fmt.Sprint(action.Command))
 						if s.IsDryRun {
-							slog.Info("dry run: [" + action.Command + "]")
+							slog.Info("dry run: " + action.Command)
 						} else {
-							// if err := execCommand(action.Command); err != nil {
-							// 	slog.Error("command execution failed: [" + action.Command + "]")
-							// 	slog.Error(err.Error())
-							// }
+							cmd := exec.Command("bash", "-c", action.Command)
+							var stdout strings.Builder
+							var stderr strings.Builder
+							cmd.Stdout = &stdout
+							cmd.Stderr = &stderr
+							if err := cmd.Run(); err != nil {
+								slog.Error("command execution failed: " + action.Command)
+								slog.Error(err.Error())
+							}
+							slog.Info("stdout: " + stdout.String())
+							if stderr.String() == "" {
+								slog.Info("stderr: " + stderr.String())
+							} else {
+								slog.Error("stderr: " + stderr.String())
+							}
 						}
 					}
 				}
 			} else {
-				slog.Error("no alert name label [" + alertNameLabel + "], skipping: " + fmt.Sprintf("%+v", alert))
+				slog.Error("no alert name label '" + alertNameLabel + "', skipping: " + fmt.Sprintf("%+v", alert))
 			}
 		}
 	}
